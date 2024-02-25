3f35cd6d15330b1e899ac3b967aa7ccae215e116
ziangold <ziangold1@gmail.com>
2024-02-26 00:24:19 +0500

    i added some java codes

--- JAVASCRIPT/First code.js
+++ JAVASCRIPT/First code.js
@@ -1,7 +1,7 @@
-// console.log("Hello World");
+// console.log("Hello World")
 
 
-// console.log("%cHello, World", "color: blue; font-size: 40px; background-color: red;");
+// console.log("%cHello, World", "color: blue; font-size: 40px; background-color: red;")
 
 
 // "john"
@@ -32,7 +32,104 @@
 // console.log(petcat , "now says " + catsound)
 
 
-// var petDog = 'Rex'; // Task 1 solution
-// console.log(petDog);
+// var petDog = 'Rex' // Task 1 solution
+// console.log(petDog)
+
+
+// var number = 123
+// console.log(number)
+// number
+// var currentTime = 7
+// console.log(true || false)
+
+// var petHungry = true
+// console.log('Feeding the pet')
+// console.log("Pet is hungry: ", !petHungry)
+// console.log(petHungry)
+
+// console.log(22 % 5) // 2
+
+
+// console.log(5 != "5")
+
+
+// var result = 50;
+
+// if(result > 40){
+//     console.log("You have passed")
+// }
+// else{
+//     console.log("You did not pass the test")
+// }
+
+
+// var place = "first";
+
+// if(place == 'first'){
+//     console.log('Gold')
+// } 
+// else if(place == 'second'){
+//     console.log('Silver')
+// }
+// else if(place == 'Third'){
+//     console.log('Bronze')
+// }
+// else{
+//     console.log('No medal')
+// }
+
+
+// var place = 'first';
+// switch(place){
+//     case 'first':
+//         console.log('Gold');
+//         break;
+//     case 'Second':
+//         console.log('Silver');
+//         break;
+//     case 'Third':
+//         console.log('Bronze');
+//         break;
+//     default:
+//         console.log('No medal');
+// }
+
+
+// for ( i = 0; i <= 3; i++) {
+//     console.log(i)
+// }
+
+
+// var i = 10;
+
+// while(i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// for(var month = 1; month < 3; month++)
+// {
+//     console.log("Month: "+ month)
+//     for(var week = 1; week <= 4; week++)
+//     {
+//         console.log("Week:", week)
+//         for(var day = 1; day <= 7; day++)
+//         {
+//             console.log("Day:", day)
+//         } 
+//     }
+// }
+
+
+// for (var month = 1; month < 3; month++) {
+//     console.log("Month: " + month);
+//     for (var week = 1; week <= 4; week++) {
+//         console.log("Week:", week);
+//         for (var day = 1; day <= 7; day++) {
+//             console.log("Day:", day);
+//         }
+//     }
+// }
 
 
